c579bfe162c7776c653fb3768aa0a5e6b141cac0
Frederik Schmitt <fschmitt@mail.uni-mannheim.de>
2016-11-16 19:59:05 +0100

    add sass and bootstrap 3

--- frontend/app/app.module.ts
+++ frontend/app/app.module.ts
@@ -1,9 +1,9 @@
- import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
- import { NgModule }      from '@angular/core';
- import { BrowserModule } from '@angular/platform-browser';
- import { AppComponent }   from './app.component';
+ import {NgModule}      from '@angular/core';
+ import {BrowserModule} from '@angular/platform-browser';
+ import {AppComponent}   from './app.component';
+
  @NgModule({
-   imports:      [ BrowserModule, NgbModule.forRoot() ],
+   imports:      [ BrowserModule],
    declarations: [ AppComponent ],
    bootstrap:    [ AppComponent ]
  })
